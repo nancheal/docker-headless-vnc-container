c30b879dbff4c578e629dea6e96393a31fb35235
nancheal <nancheal@126.com>
2018-03-18 09:29:26 +0800

    docker4burp

--- src/centos/install/burpsuite_pro.sh
+++ src/centos/install/burpsuite_pro.sh
@@ -2,9 +2,5 @@
 set -e
 
 echo "Install burpsuite pro"
-yum install -y java-1.8.0-openjdk.x86_64
-yum install -y unzip
 wget https://down.52pojie.cn/Tools/Network_Analyzer/Burp_Suite_Pro_v1.7.32_Loader_Keygen.zip
 unzip Burp_Suite_Pro_v1.7.32_Loader_Keygen.zip
-yum clean all
-java -jar burp-loader-keygen.jar
--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -7,4 +7,6 @@ yum -y install epel-release
 yum -y update
 yum -y install vim sudo wget which net-tools bzip2 \
     numpy #used for websockify/novnc
+yum install -y java-1.8.0-openjdk.x86_64
+yum install -y unzip
 yum clean all
