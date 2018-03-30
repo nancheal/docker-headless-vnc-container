026a806f016f6dae223fb7b26b0939ddbc4db13e
nancheal <nancheal@126.com>
2018-03-30 21:34:48 +0800

    docker4burp

--- src/centos/install/burpsuite_pro.sh
+++ src/centos/install/burpsuite_pro.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Install burpsuite pro"
 wget https://down.52pojie.cn/Tools/Network_Analyzer/Burp_Suite_Pro_v1.7.33_Loader_Keygen.zip
-unzip Burp_Suite_Pro_v1.7.32_Loader_Keygen.zip
+unzip Burp_Suite_Pro_v1.7.33_Loader_Keygen.zip
